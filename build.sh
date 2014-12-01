a25ccc1ae013c5b3f0c70d985a9c883111d3a933
software-engineering <ubuntu@engineering.(none)>
2014-11-30 23:24:39 -0700

    added h2 jar to classpath on build

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ rm -rf bin
 mkdir bin 2>/dev/null
 
 find src -name "*.java" |grep -v "Test.java" > sources.txt
-javac -source 1.6 -d bin -cp bin @sources.txt
+javac -source 1.6 -d bin -cp 'bin:lib/h2-1.4.182.jar' @sources.txt
 cp -r ./resource ./bin/resource
 cp -r ./lib ./bin/lib
-
